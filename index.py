1e22046f78505ba81d1e14b890fa8336c51717d7
Vladimir Gutierrez <vladimirgutierrez4063@gmail.com>
2021-03-31 20:33:05 -0300

    second commit

--- index.py
+++ index.py
@@ -16,5 +16,14 @@ def contacto():
 def principal():
     return render_template('index.html')
 
+@app.route('/lenguajes')
+def lenguajes():
+    misLengujes = ("PHP", "Python", "Java", "JavaScript", "Cobol", "Dart", "Ruby")
+    return render_template('lenguajes.html', lenguajes = misLengujes) #"lengujes" es el parametro puente a base.html
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=5010)
